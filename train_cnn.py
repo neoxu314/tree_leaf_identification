9f3122b70498770d6c63ac0b421aec0b0a279576
Neo <825985679@qq.com>
2017-11-20 16:12:45 +1300

    Update train_cnn.py

--- train_cnn.py
+++ train_cnn.py
@@ -20,13 +20,6 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import SGD, Adam
 
-
-# map_characters = {0: 'abraham_grampa_simpson', 1: 'apu_nahasapeemapetilon', 2: 'bart_simpson',
-#         3: 'charles_montgomery_burns', 4: 'chief_wiggum', 5: 'comic_book_guy', 6: 'edna_krabappel',
-#         7: 'homer_simpson', 8: 'kent_brockman', 9: 'krusty_the_clown', 10: 'lisa_simpson',
-#         11: 'marge_simpson', 12: 'milhouse_van_houten', 13: 'moe_szyslak',
-#         14: 'ned_flanders', 15: 'nelson_muntz', 16: 'principal_skinner', 17: 'sideshow_bob'}
-
 map_characters = {0: 'tree_1', 1: 'tree_2'}
 
 pic_size = 64
